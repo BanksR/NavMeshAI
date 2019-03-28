ebfe6072f2ccb34c45ccf86c8844d5749ea927b8
Richard <r.banks@burnley.ac.uk>
2019-03-28 11:41:19 +0000

    Added some waypoint stuff for phoebe

---
+++ NavMeshProject/Assets/Scripts/IColorable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IColorable
+{
+
+	void ChangeColor(Color col);
+}
---
+++ NavMeshProject/Assets/pickUpObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpObject : MonoBehaviour
+{
+	public GameObject particles;
+
+	void Awake()
+	{
+		GameManager._pickUpObjects.Add(this);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("UFO"))
+		{
+			//Ufo.targID++;
+			GameManager.instance.RemoveAndResort(this);
+			this.gameObject.SetActive(false);
+			
+			
+			GameManager.instance.ParticleSpawner(particles, this.transform.position);
+		}
+	}
+}
